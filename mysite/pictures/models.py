94ae849f43a1e8f346236f6b4bf78ea92e12ca36
Hafiz <hafiz-2010@hotmail.co.uk>
2021-08-16 14:25:23 +0100

    initial commit

---
+++ mysite/pictures/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from datetime import datetime
+
+class Photo(models.Model):
+    image = models.ImageField(height_field=None, width_field=None, max_length=100, blank=True)
+    dateTaken = models.DateField(auto_now=False, auto_now_add=False, default=datetime.now, verbose_name='The date and time the photo was taken')
+    takenBy = models.CharField(max_length=100, verbose_name='The person which has taken the photo')
+    appprovedDate = models.DateField(auto_now=True, auto_now_add=True, verbose_name="The date and time the photo was approved")
+    def __str__(self):
+        return self.image.name
